b1c67a36c0f6b5c8797f64888765ef3fb0bb5008
Li <2655884083@qq.com>
2019-04-23 11:33:22 +0800

    Initial commit

---
+++ app/src/main/java/com/lyp/crime/Crime.java
@@ -0,0 +1,47 @@
+package com.lyp.crime;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * @author Lyp
+ * @create 2019/4/23
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
---
+++ app/src/main/java/com/lyp/crime/CrimeActivity.java
@@ -0,0 +1,16 @@
+package com.lyp.crime;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * @author Lyp
+ * @create 2019/4/23
+ */
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
